2b75d8c6cde0e9127c7e8096474175c46a0ccd01
Dante Luchini <danteluchini@gmail.com>
2023-11-01 11:07:32 -0300

    Added a function to deploy the answer in the FAQ section and removed the title change in the nav

--- script.js
+++ script.js
@@ -1,39 +1,39 @@
 // Gallery slides
 
-document.addEventListener("DOMContentLoaded", function() {
-    const img = document.querySelectorAll(".gallerygoDent img");
-    const fulImg = document.getElementById("fulImg");
-    let currentSrc = fulImg.src;
-  
-    function prevImg() {
-      for (let i = 0; i < img.length; i++) {
-        if (img[i].src === currentSrc) {
-          const prevIndex = (i - 1 + img.length) % img.length;
-          fulImg.src = img[prevIndex].src;
-          currentSrc = img[prevIndex].src;
-          break;
-        }
+document.addEventListener("DOMContentLoaded", function () {
+  const img = document.querySelectorAll(".gallerygoDent img");
+  const fulImg = document.getElementById("fulImg");
+  let currentSrc = fulImg.src;
+
+  function prevImg() {
+    for (let i = 0; i < img.length; i++) {
+      if (img[i].src === currentSrc) {
+        const prevIndex = (i - 1 + img.length) % img.length;
+        fulImg.src = img[prevIndex].src;
+        currentSrc = img[prevIndex].src;
+        break;
       }
     }
-  
-    function nextImg() {
-      for (let i = 0; i < img.length; i++) {
-        if (img[i].src === currentSrc) {
-          const nextIndex = (i + 1) % img.length;
-          fulImg.src = img[nextIndex].src;
-          currentSrc = img[nextIndex].src;
-          break;
-        }
+  }
+
+  function nextImg() {
+    for (let i = 0; i < img.length; i++) {
+      if (img[i].src === currentSrc) {
+        const nextIndex = (i + 1) % img.length;
+        fulImg.src = img[nextIndex].src;
+        currentSrc = img[nextIndex].src;
+        break;
       }
     }
-  
-    const rightArrow = document.querySelector(".arrow.right");
-    const leftArrow = document.querySelector(".arrow.left");
-  
-    rightArrow.addEventListener("click", nextImg);
-    leftArrow.addEventListener("click", prevImg);
-  });
-  
+  }
+
+  const rightArrow = document.querySelector(".arrow.right");
+  const leftArrow = document.querySelector(".arrow.left");
+
+  rightArrow.addEventListener("click", nextImg);
+  leftArrow.addEventListener("click", prevImg);
+});
+
 // Open modal
 const fulImgBox = document.getElementById("fulImgBox");
 const fulImg = document.getElementById("fulImg");
@@ -49,20 +49,20 @@ function closeImg() {
 
 // No gallery
 
-window.addEventListener('DOMContentLoaded', function() {
-    var check = document.getElementById('check');
-    var gallery = document.querySelector('.gallery');
-  
-    check.addEventListener('change', function() {
-      if (this.checked) {
-        gallery.style.display = 'none';
-      } else {
-        gallery.style.display = 'block';
-      }
-    });
-  });
+window.addEventListener('DOMContentLoaded', function () {
+  var check = document.getElementById('check');
+  var gallery = document.querySelector('.gallery');
 
-  
+  check.addEventListener('change', function () {
+    if (this.checked) {
+      gallery.style.display = 'none';
+    } else {
+      gallery.style.display = 'block';
+    }
+  });
+});
+
+
 // Change active header 
 
 const homeH = document.getElementById("homeHeader");
@@ -70,16 +70,51 @@ const staffH = document.getElementById("staffHeader");
 const faqH = document.getElementById("faqHeader");
 const contactH = document.getElementById("contactHeader");
 const vecHeader = document.querySelectorAll(".info");
-const titleElement = document.getElementById("tittleH");
+
 
 vecHeader.forEach(headerElement => {
-    headerElement.addEventListener("click", () => {
+  headerElement.addEventListener("click", () => {
 
-        const newTitle = headerElement.textContent;
-        titleElement.textContent = newTitle;
-        vecHeader.forEach(btn => {
-            btn.classList.remove("active");
-        });
-        headerElement.classList.add("active");
+
+    vecHeader.forEach(btn => {
+      btn.classList.remove("active");
     });
+    headerElement.classList.add("active");
+  });
 });
+
+
+// Show FAQ anwers 
+
+const btnShow = document.querySelectorAll(".btnFaq");
+const answers = document.querySelectorAll(".answer");
+const cont = document.querySelectorAll(".faqSons");
+
+btnShow.forEach((btn, index) => {
+  btn.addEventListener("click", () => {
+    const answer = answers[index];
+    const container = cont[index];
+
+    if (!answer.classList.contains("visible")) {
+      answer.classList.add("visible");
+      container.style.maxHeight = container.scrollHeight + "px";
+      container.style.transition = "max-height 0.5s ease-in-out";
+      btn.textContent = "-";
+    } else {
+      container.style.transition = "max-height 0.5s ease-in-out";
+      container.style.maxHeight = "70px"; 
+      btn.textContent = "+";
+
+      
+      setTimeout(() => {
+        answer.classList.remove("visible");
+      }, 500); 
+    }
+  });
+});
+
+
+
+
+ 
+
